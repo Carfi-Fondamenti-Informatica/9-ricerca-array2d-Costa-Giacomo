a977c806d44b0949875850b953d5678a62222de6
Costa-Giacomo <92380404+Costa-Giacomo@users.noreply.github.com>
2021-12-11 11:58:07 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,13 @@
+#include "lib.h"
+#include <cstring>
 
+bool cerca (char elenco[10][20], char nome[1][20], int &posizione){
+
+    for(int i=0; i<10; i++){
+        if (strcmp (elenco[i], nome[0]) == 0){
+            posizione=i;
+            return true;
+        }
+    }
+    return false;
+}
